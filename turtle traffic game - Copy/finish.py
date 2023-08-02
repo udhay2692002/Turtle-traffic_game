6f4e5b539456077b86d0fe63efd21c34d0de6f18
Udhaya Kumar <140731595+udhay2692002@users.noreply.github.com>
2023-08-02 22:21:42 +0530

    Add files via upload

---
+++ turtle traffic game - Copy/finish.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+
+
+class Finish(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.finish_point()
+
+
---
+++ turtle traffic game - Copy/scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level=1
+        self.penup()
+        self.hideturtle()
+        self.goto(-280,250)
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.write(f"LEVEL:{self.level}", align="left", font=FONT)
+
+    def increase_score(self):
+        self.clear()
+        self.level+=1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER",align="center",font=FONT)
+
+
+
+
+
+
+
+
+
+
